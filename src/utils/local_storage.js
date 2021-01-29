f0c549a6c8a313ec46224e317053fb35963b9988
mahmudahmed <mahmudahmednyc@gmail.com>
2021-01-29 13:56:55 -0500

    Readme Local Storage

--- src/utils/local_storage.js
+++ src/utils/local_storage.js
@@ -1,13 +1,18 @@
+// save all tasks to local storage 
 export const saveToLocalStorage = (key, data) => localStorage.setItem(key, JSON.stringify(data));
 
+// fetch all tasks from local storage
 export const fetchFromLocalStorage = key => {
+    let todoItems = [];
     const value = localStorage.getItem(key); 
 
-    let todoItems = [];
-
+    // if fetch was able to retrive data 
     if (value) {
         const parsedJSON = JSON.parse(value);
         if (Array.isArray(parsedJSON)) { todoItems = parsedJSON }
     } 
     return todoItems;
 }
+
+// clear data in local storage 
+export const clearLocalStorage = key => localStorage.removeItem(key);
