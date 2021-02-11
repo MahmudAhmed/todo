2153f92fc9b43586d4ab7b115fcca98d41a2eb15
mahmudahmed <md16ahmed@gmail.com>
2021-02-11 16:35:55 -0500

    login complete

--- src/components/login_modal.jsx
+++ src/components/login_modal.jsx
@@ -1,19 +1,22 @@
 import React, { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
+import { faSignOutAlt, faLock, faUser } from '@fortawesome/free-solid-svg-icons';
 import axios from 'axios';
-window.axios = axios;
 
 function LoginModal() {
   const [display, toggleDisplay] = useState(true);
   const [email, changeEmail] = useState("");
   const [password, changePassword] = useState("");
   const [disabled, toggleDisabled] = useState(false);
-  const [errors, setErrors] = useState([]);
+  const [authErrors, setAuthErrors] = useState([]);
+  const [emailErrors, setEmailErrors] = useState([]);
+  const [passwordErrors, setPasswordErrors] = useState([]);
 
   const handleAuth = () => {
     toggleDisplay(n => !n);
-    setErrors("");
+    setAuthErrors([]);
+    setEmailErrors([]);
+    setPasswordErrors([]);
     changePassword("");
     changeEmail("");
   };
@@ -24,35 +27,72 @@ function LoginModal() {
       <div className={showHideClassName}>
         <section className="modal-content">
           {loginForm()}
-          {/* <button id="modal-close-btn" type="button" onClick={handleAuth}>Done</button> */}
         </section>
       </div>
     )
   }
 
+  const handleEmailValidation = () => {
+    const emialErrs = [];
+    if (email === "") {
+      emialErrs.push("Email field cannot be blank")
+    } else if (email.length >= 50) {
+      emialErrs.push("Email should be less than 50 characters long")
+    }
+    
+    setEmailErrors(emialErrs);
+
+    return emialErrs.length > 0;
+  }
+
+  const handlePasswordValidation = () => {
+    const passErrs = [];
+ 
+    if (password === "") {
+      passErrs.push("Password field cannot be blank")
+    } else if (password.length < 4) {
+      passErrs.push("Password must be atleast 4 characters long")
+    }
+
+    setPasswordErrors(passErrs);
+
+    return passErrs.length > 0;
+  }
+
+  // we break this down make sure to call both validation functions to get errors for email & password at the same time. 
+  // () => handleEmailValidation() || handlePasswordValidation(); => only updates emails errors only becuase of truthy value
+  const formHasErrors = () => {
+    let hasErrors = false;
+    if (handleEmailValidation()) hasErrors = true;
+    if (handlePasswordValidation()) hasErrors = true;
+    return hasErrors
+  }
+
 
   const handleFormSubmission = async (e) => {
     e.preventDefault();
-    debugger
-    if (email === "" || password === "") {
-      setErrors(["Email/Password field cannot be blank!"])
-    } else if (password.length < 4) {
-      setErrors(["Password must be atleast 4 characters long!"])
-    } else {
+    setAuthErrors([]);
+
+    if (!formHasErrors()) {
       toggleDisabled(true)
       var data = new FormData();
       data.append("email", email)
       data.append("password", password)
       await axios.post(`https://dev.rapptrlabs.com/Tests/scripts/user-login.php`, data)
         .then(res => {
-          debugger
           toggleDisabled(false);
           handleAuth();
         }, err => {
-          debugger
-          setErrors([err.response.data.message])
+          setAuthErrors([err.response.data.message])
           toggleDisabled(false)
         })
+    }
+
+  }
+
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      handleFormSubmission(e);
     }
   }
 
@@ -62,31 +102,41 @@ function LoginModal() {
       <div className="login-form-container">
         <h2 id="form-title">Rapptr Labs</h2>
         <form onSubmit={handleFormSubmission} >
-          <p id="form-errors">{errors}</p>
+          <p id="form-errors">{authErrors}</p>
           <label className="form-label">
             Email:
           </label>
-          <input
-            className="login-field"
-            type="email"
-            name="email"
-            value={email}
-            placeholder={"user@rapptrlabs.com"}
-            onChange={e => changeEmail(e.target.value)}
-            disabled={disabled}
-          />
+          <div className="form-input-container">
+            <FontAwesomeIcon icon={faUser} id="user-icon" />
+            <input
+              className={`login-field ${emailErrors.length > 0 ? "invalid" : ""}`}
+              type="email"
+              name="email"
+              value={email}
+              placeholder={"user@rapptrlabs.com"}
+              onChange={e => { changeEmail(e.target.value); handleEmailValidation();}}
+              disabled={disabled}
+              onKeyPress={handleKeyPress}
+            />
+          </div>
+          {emailErrors.map( err => <p className="input-errors">{err}</p>)}
           <label className="form-label">
             Password:
           </label>
-          <input
-            className="login-field"
-            type="password"
-            name="password"
-            value={password}
-            placeholder={"Must be at least 4 characters"}
-            onChange={e => changePassword(e.target.value)}
-            disabled={disabled}
-          />
+          <div className="form-input-container">
+            <FontAwesomeIcon icon={faLock} id="lock-icon" />
+            <input
+              className={`login-field ${passwordErrors.length > 0 ? "invalid" : ""}`}
+              type="password"
+              name="password"
+              value={password}
+              placeholder={"Must be at least 4 characters"}
+              onChange={e => { changePassword(e.target.value); handlePasswordValidation();}}
+              disabled={disabled}
+              onKeyPress={handleKeyPress}
+            />
+          </div>
+          {passwordErrors.map(err => <p className="input-errors">{err}</p>)}
           <button disabled={disabled} type="submit" id="login-btn" >Login</button>
         </form>
       </div>
@@ -95,7 +145,7 @@ function LoginModal() {
 
   return (
     <div className="modal-container logout-modal">
-      <button onClick={handleAuth}><FontAwesomeIcon icon={faSignOutAlt} id="user-icon" /></button>
+      <button title="Sign Out" onClick={handleAuth}><FontAwesomeIcon icon={faSignOutAlt} id="logout-icon" /></button>
       { modal()}
     </div>
   );
